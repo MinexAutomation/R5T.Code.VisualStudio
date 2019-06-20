ea8e5bde01656a08cb6e8e7036e53376da90175c
MinexAutomation <minex.automation@gmail.com>
2019-06-20 14:07:17 -0400

    Added solution- and project-file information service interfaces.

---
+++ source/R5T.Code.VisualStudio/Code/Services/Interfaces/IProjectFileInformationServicesProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using R5T.Code.VisualStudio.IO;
+
+
+namespace R5T.Code.VisualStudio
+{
+    public interface IProjectFileInformationServicesProvider
+    {
+        ProjectFilePath[] GetReferencedProjectFilePaths(ProjectFilePath projectFilePath);
+        ProjectFilePath[] GetReferencedProjectFilePathsRecursive(ProjectFilePath projectFilePath);
+
+        /// <summary>
+        /// Determines if a project file has a version, and if so, returns the version.
+        /// If the project file does not have a version, returns <see cref="R5T.NetStandard.VersionHelper.None"/>.
+        /// </summary>
+        bool HasVersion(ProjectFilePath projectFilePath, out Version version);
+    }
+}
